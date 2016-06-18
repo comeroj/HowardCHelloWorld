1a8185864c72687669558d085e12aa143affa3fe
Josh Pendergrass <comeroj@hotmail.com>
2016-06-18 16:12:52 -0700

    adding howards masterpiece

---
+++ HowardsConsoleApplication/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowardsConsoleApplication
+{
+    class Person
+    {
+
+
+        public Person()
+        {
+            LastName = "Brown";
+        }
+        public Person(string fname, string lname)
+        {
+            FirstName = fname;
+            LastName = lname;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstname;
+            }
+            set
+            {
+                _firstname = value;
+                _fullname = null;
+            }
+        }
+        private string _firstname = "Howard";
+        public string LastName { get; set; }
+        public bool hadadog = false;
+        public int age = 3;
+        public long secondsalive = 168465189;
+        public double moneyinthebank = 15.21;
+        decimal pie = 3.1405168161M;
+        public string[] nicknames = { "abe", "jones" };
+        public DateTime _today = DateTime.Now;
+        public Guid? myguid = Guid.NewGuid();
+        private string _fullname;
+        public string Fullname
+        {
+            get
+            {
+                if (_fullname == null)
+                {
+                    _fullname = FirstName + " " + LastName;
+                }
+
+                return _fullname;
+            }
+        }
+    }
+}
--- HowardsConsoleApplication/Program.cs
+++ HowardsConsoleApplication/Program.cs
@@ -8,8 +8,25 @@ namespace HowardsConsoleApplication
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+
+            Person myperson = new Person();
+            Person myperson2 = new Person("Jim", "Jones");
+            Person[] mypeeps = { myperson, myperson2 };
+            foreach (Person peep in mypeeps)
+            {
+                Console.WriteLine("Hello " + peep.Fullname + ",");
+            }
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine(i);
+                i += 1;
+                if (i > 200) { break; }
+            }
         }
     }
 }
